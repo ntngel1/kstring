f2483f1a484ebc1b4108910c72a947d5c9d54126
Kirill <ntngel1@gmail.com>
2019-04-12 20:37:26 +0300

    Insert

--- kstring.c
+++ kstring.c
@@ -29,30 +29,33 @@ void kstring_free(kstring_t* instance)
 kstring_t* kstring_dup(const char* string) {
    size_t length = string_length(string);
    kstring_t* instance = kstring_create(length);
+
    memcpy(instance->data, string, length);
    instance->how_much = length;
+   
    return instance;
 }
 
 char* kstring_cstr(const kstring_t* instance)
 {
     char* str = malloc(instance->length + 1);
+
     memcpy(str, instance->data, instance->length);
     str[instance->length] = '\0';
+
     return str;
 }
 
 void kstring_copy(const kstring_t* source, kstring_t** destination)
 {
     kstring_t* dest = *destination;
-    if (dest->length < source->length)
+
+    if (dest->length < source->length || dest->length > source->length)
     {
-        kstring_free(dest);
-        dest = kstring_create(source->length);
+        dest->data = realloc(dest->data, source->length * sizeof(uint8_t));
     }
     
     dest->how_much = source->length;
-    
     memcpy(dest->data, source->data, source->length);
 }
 
@@ -139,6 +142,11 @@ kstring_t* kstring_slice(const kstring_t* source, size_t begin, size_t end)
         return NULL;
     }
 
+    if (begin == end)
+    {
+        return NULL;
+    }
+
     if (begin > end)
     {
         size_t temp = begin;
@@ -149,18 +157,35 @@ kstring_t* kstring_slice(const kstring_t* source, size_t begin, size_t end)
     kstring_t* destination = kstring_create(end - begin);
     destination->how_much = destination->length;
     memcpy(destination->data, source->data, destination->length);
+    
     return destination;
 }
 
-kstring_t* kstring_append(const kstring_t* left, const kstring_t* right)
+void kstring_append(kstring_t** instance, const char* string)
 {
-    kstring_t* instance = kstring_create(left->length + right->length);
-    instance->how_much = left->length + right->length;
+    size_t string_len = string_length(string);
+    if (string_len == 0)
+    {
+        return;
+    }
 
-    memcpy(instance->data, left->data, left->length);
-    memcpy(instance->data + left->length, right->data, right->length);
+    kstring_t* dereferenced = *instance;
 
-    return instance;
+    dereferenced->data = realloc(dereferenced->data, string_len + dereferenced->length);
+    memcpy(dereferenced->data + dereferenced->length, string, string_len);
+    dereferenced->length = string_len + dereferenced->length;
+    dereferenced->how_much = dereferenced->length;
+}
+
+uint8_t kstring_at(const kstring_t* instance, size_t position)
+{
+    if (position >= instance->length)
+    {
+        fprintf(stderr, "Error accessing element at index(%ld) in kstring(address = %p, length = %ld)\n", position, instance, instance->length);
+        return 0;
+    }
+
+    return instance->data[position];
 }
 
 size_t* __kstring_horspool_build_offset_table__(const char* string, size_t length)
@@ -178,17 +203,6 @@ size_t* __kstring_horspool_build_offset_table__(const char* string, size_t lengt
     }
 
     return offset;
-}
-
-uint8_t kstring_at(const kstring_t* instance, size_t position)
-{
-    if (position >= instance->length)
-    {
-        fprintf(stderr, "Error accessing element at index(%ld) in kstring(address = %p, length = %ld)\n", position, instance, instance->length);
-        return 0;
-    }
-
-    return instance->data[position];
 }
 
 size_t kstring_find(const kstring_t* instance, const char* needle)
@@ -217,11 +231,13 @@ size_t kstring_find(const kstring_t* instance, const char* needle)
 
             if (i == 0)
             {
+                free(offsets);
                 return skip + i;
             }
         }
     }
 
+    free(offsets);
     return -1;
 }
 
@@ -229,7 +245,6 @@ void kstring_trim(kstring_t* instance)
 {
     size_t counter = 0;
     size_t whitespaceCount = 0;
-    bool hasWhitespaces = false;
     
     for (size_t i = 0; i < instance->length; ++i)
     {
@@ -238,32 +253,98 @@ void kstring_trim(kstring_t* instance)
             instance->data[counter++] = instance->data[i];
         } else {
             whitespaceCount++;
-            hasWhitespaces = true;
         }
     }
 
-    if (!hasWhitespaces)
+    if (whitespaceCount == 0)
         return;
 
-    size_t length = instance->length - whitespaceCount;
-    instance->length = length;
-    instance->how_much = length;
-
-    char* buffer = malloc(sizeof(uint8_t) * length);
-    memcpy(buffer, instance->data, length);
-    free(instance->data);
-    instance->data = buffer;
+    instance->length = counter;
+    instance->how_much = counter;
+    instance->data = realloc(instance->data, counter);
 }
+
+size_t kstring_lastIndexOf(kstring_t* instance, char character)
+{
+    for (size_t i = instance->length - 1; i >= 0; --i)
+    {
+        if (instance->data[i] == character)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+void kstring_reverse(kstring_t* instance)
+{
+    size_t a = 0;
+    size_t b = 0;
+    for (a = 0, b = instance->length - 1; a < instance->length && b >= 0; ++a, --b)
+    {
+        printf("Normal = %c, reversed = %c\n", instance->data[a], instance->data[b]);
+        char left_char = instance->data[a];
+        char right_char = instance->data[b];
+        instance->data[a] = right_char;
+        instance->data[b] = left_char;
+    }
+}
+
+void kstring_test(kstring_t* instance)
+{
+    printf("TEST RUNNED\n");
+
+    if (instance->length > instance->how_much || instance->length < instance->how_much)
+    {
+        printf("Warning! Defined length = %ld, but defined how_much = %ld\n", instance->length, instance->how_much);
+    }
+
+    const char* string = kstring_cstr(instance);
+    const size_t str_len = string_length(string);
+
+    if (str_len > instance->how_much || str_len < instance->how_much)
+    {
+        printf("Warning! Real string length = %ld, but defined how_much = %ld\n", str_len, instance->how_much);
+    }
+
+    if (str_len > instance->length || str_len < instance->length)
+    {
+        printf("Warning! Real string length = %ld, but defined length = %ld\n", str_len, instance->length);
+    }
+}
+
+void kstring_insert(kstring_t** instance, size_t position, const char* string) {
+    size_t string_len = string_length(string);
+    kstring_t* dereferenced = *instance;
+    if (position > dereferenced->length - 1) 
+    {
+        printf("Warning! Trying to insert string(%s) at position(%ld) but instance->length(%ld)\n", string, position, dereferenced->length);
+    }
+
+    if (position == dereferenced->length - 1)
+    {
+        kstring_append(instance, string);
+    }
+
+    // TODO Complete
+}
+
+
+
+
+
 
 int main(int argc, const char** argv)
 {
     kstring_t* hello = kstring_dup("Hello");
-    kstring_t* world = kstring_dup(" Wo rl d! ");
-    kstring_trim(world);
-    kstring_print(world);
+    kstring_append(&hello, "W O R L DO    ");
+    kstring_t* concated = kstring_concat(hello, hello);
+    kstring_test(concated);
+    kstring_test(hello);
 
-    kstring_free(hello);
-    kstring_free(world);
+    hello = kstring_slice(hello, 0, 5);
+    kstring_test(hello);
 
     return 0;
 }
